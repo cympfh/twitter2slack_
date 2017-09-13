3803bcd986fcec02eaeaeaa2e8e15dd86cc87f37
whoami <whose@mail.ai>
2017-09-13 16:41:42 +0900

    embedding text only if protected

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ function post(tweet, debug=false) {
     id = tweet.id_str
     post_url = `https://twitter.com/${username}/status/${id}`
 
+    text = post_url
+    if (tweet.user.protected) {
+        text += "\n:key: " + tweet.text;
+    }
+
     headers = {
         "Content-type": "application/json"
     };
@@ -24,7 +29,7 @@ function post(tweet, debug=false) {
         "icon_url": icon_url,
         "fallback": "Hi",
         "channel": "#timeline_twitter",
-        "text": post_url
+        "text": text
     }
     options = {
         headers: headers,
@@ -50,7 +55,7 @@ function main(debug=false) {
     client.stream('user', {}, (stream) => {
         stream.on('data', (tweet) => {
             username = tweet.user.screen_name;
-            console.log(username, tweet.entities.urls, `black?=${is_black(username)}`);
+            console.log(username, `black?=${is_black(username)}`);
             if (is_black(username)) return;
             console.log(tweet);
             post(tweet);
