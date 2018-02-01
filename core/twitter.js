803170df44faeec911f7f7b6ad309e305b556ddf
cympfh <cympfh@gmail.com>
2018-02-01 14:38:52 +0900

    hoge

--- core/twitter.js
+++ core/twitter.js
@@ -1,7 +1,8 @@
 function getName(tweet) {
     const name = tweet.user.name;
-    const username = tweet.user.screen_name;
-    return `${name} @${username}`;
+    return name;
+    // const username = tweet.user.screen_name;
+    // return `${name} @${username}`;
 }
 
 module.exports = {
